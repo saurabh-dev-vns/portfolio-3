a638b6bd7f62c2865fc034902a586251e2fad195
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-23 21:01:20 +0530

    navbar update

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-   <div className='z-10'>
-    <h1 className='text-black inline-block fixed left-8 top-8'>SS</h1>
-    <a href="#" className='text-black absolute top-8 right-[calc(1rem+2vw)]'>Say hi..</a>
-    <a href="#" className='text-black absolute top-2/4 transform -rotate-90'>Projects</a>
-    <a href="#" className='text-black absolute top-2/4 right-0 transform rotate-90'>Projects</a>
-   <div className='absolute bottom-4 flex justify-evenly w-full'>
-   <a href="#" className='text-black '>About</a>
-   <a href="#" className='text-black '>Skill</a>
+    <div className='z-10'>
+      <h1 className='text-black inline-block fixed left-8 top-8'>SS</h1>
+      <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500 absolute top-8 right-[calc(1rem+2vw)]'>Say hi..</a>
+      <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500 absolute top-2/4  -rotate-90'>Projects</a>
+      <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem]  duration-500  absolute top-2/4 right-0 transform rotate-90'>Projects</a>
+      <div className='absolute bottom-4 flex justify-evenly w-full'>
+        <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500'>About</a>
+        <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500'>Skill</a>
+      </div>
     </div>
-   </div>
   );
 };
 
