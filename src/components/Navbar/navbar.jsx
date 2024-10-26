8b74d1443d85917e0b00f2a4cb5dd6405d791c32
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-26 20:05:40 +0530

    some changes

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,15 +1,17 @@
-import { div } from 'framer-motion/client';
 import React from 'react';
+import { useGlobalContext } from "../../hooks/useGlobalContext";
 
 const Navbar = () => {
+  const { isClicked, setIsClicked } = useGlobalContext();
+
   return (
     <div className='z-10'>
-      <h1 className='text-black inline-block fixed left-8 top-8'>SS</h1>
-      <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500 absolute top-8 right-[calc(1rem+2vw)]'>Say hi..</a>
-      <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500 absolute top-2/4  -rotate-90'>Projects</a>
-      <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem]  duration-500  absolute top-2/4 right-0 transform rotate-90'>Projects</a>
+      <h1 className={`${isClicked?'text-white':'text-black'} inline-block fixed left-8 top-8`}>SS</h1>
+      <a href="#" className={`${isClicked?"text-white md:text-black":"text-black"} font-medium text-xl hover:text-[1.3rem] transform duration-500 absolute top-8 right-[calc(1rem+2vw)]`}>Say hi..</a>
+      <a href="#" className={`${isClicked?"md:text-white":"text-black"} font-medium text-xl hover:text-[1.3rem] transform duration-500 absolute top-[calc(50%+30px)] md:top-2/4 -rotate-90`}>Projects</a>
+      <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem]  duration-500  absolute top-[calc(50%+30px)] md:top-2/4 right-0 transform rotate-90'>Projects</a>
       <div className='absolute bottom-4 flex justify-evenly w-full'>
-        <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500'>About</a>
+        <a href="#" className={`${isClicked?"md:text-white":"text-black"} font-medium text-xl hover:text-[1.3rem] transform duration-500`}>About</a>
         <a href="#" className='text-black font-medium text-xl hover:text-[1.3rem] transform duration-500'>Skill</a>
       </div>
     </div>
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -12,10 +12,11 @@ const Home = () => {
         <div className="w-full h-screen flex md:flex-row flex-col relative overflow-hidden">
             <Navbar />
             <div
-                className={`absolute inset-0 bg-white flex items-center justify-center transition-opacity duration-1000 ease-in-out 
+                className={`absolute inset-0 bg-white flex items-center justify-center transition-opacity
+                    duration-1000 ease-in-out 
             ${isClicked ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}
             ></div>
-<RotatingButton/>
+            <RotatingButton />
             <div className={`black w-full md:w-6/12 md:h-screen h-3/6 bg-black transition-transform 
                 duration-1000 ease-in-out ${isClicked ? 'translate-y-0' : '-translate-y-full'}`}>
             </div>
