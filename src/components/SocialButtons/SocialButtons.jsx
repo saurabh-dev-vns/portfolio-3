6c03f3cb475f9584e67f1610c9f2db72e005412b
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-28 21:58:19 +0530

    social button update

--- src/components/SocialButtons/SocialButtons.jsx
+++ src/components/SocialButtons/SocialButtons.jsx
@@ -7,13 +7,13 @@ const SocialButtons = () => {
 
   return (
     <div className="fixed flex items-center justify-center flex-col bottom-0 left-8 z-10 gap-3">
-      <a href="#" className={` ${isClicked?'md:text-white':'md:text-black'}`}>
-        <FaLinkedinIn className="w-[25px] h-[25px] hover:scale-125 transform transition-transform duration-300" />
-      </a>
-      <a href="#" className={` ${isClicked?'md:text-white':'md:text-black'}`}>
+      <a href="https://github.com/saurabh-dev-vns" target='_blank' className={` ${isClicked?'md:text-white':'md:text-black'}`}>
         <FaGithub className="w-[25px] h-[25px] hover:scale-125 transform transition-transform duration-300" />
       </a>
-      <a href="#" className={` ${isClicked?'md:text-white':'md:text-black'}`}>
+      <a href="https://www.linkedin.com/in/saurabh-sahani-dev"  target='_blank' className={` ${isClicked?'md:text-white':'md:text-black'}`}>
+        <FaLinkedinIn className="w-[25px] h-[25px] hover:scale-125 transform transition-transform duration-300" />
+      </a>
+      <a href="https://saurabhsahani.in/"  target='_blank' className={` ${isClicked?'md:text-white':'md:text-black'}`}>
         <FaExternalLinkAlt className="w-[25px] h-[25px] hover:scale-125 transform transition-transform duration-300" />
       </a>
       <span className={`w-[2px] h-[8rem] ${isClicked?'bg-white':' bg-black '}`}></span>
