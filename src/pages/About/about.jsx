9498f46107c08e820a255600a2bdbfce6e835bf4
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-10-30 22:18:02 +0530

    about page update

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="p-8">
-      <h2 className="text-3xl font-bold">About Me</h2>
-      <p className="mt-4 text-lg">
-        Hello! I'm [Your Name], a [Your Profession]. I love [Your Interests/Skills].
-      </p>
-    </div>
+    <section
+    className={`w-full h-screen bg-black`}
+    >
+
+    </section>
   );
 };
 
